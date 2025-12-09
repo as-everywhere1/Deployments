c0c655481fd9e33502fca3b12bfa9df8631b8d99
TecheAI <git@teche.ai>
2025-12-09 19:41:54 +0530

    transfering here

---
+++ mindmap-wisemap/setup-mindmap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#read -p "Enter the URL(Do not add http or https): https://" app_url
+app_url=$(grep "^mindmap_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
+apt install wget curl docker-compose sudo -y > /dev/null
+BASE_DIR=/mnt/DriveDATA/
+mkdir -p $BASE_DIR/mindmap
+PORT=7087
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+sleep 2
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock > /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/mindmap-wisemap/mindmap-nginx.conf -o mindmap-nginx.conf
+sed -i "s|prefixmindmap.domainname|$app_url|g" ./mindmap-nginx.conf
+mv mindmap-nginx.conf  /etc/nginx/sites-available/mindmap
+ln -s /etc/nginx/sites-available/mindmap /etc/nginx/sites-enabled/mindmap
+docker run -dt --restart unless-stopped --name mindmap-wisemap -p $PORT:8080 wisemapping/wisemapping:latest
+
+#docker cp mindmap-wisemap:/var/lib/wisemapping/db $BASE_DIR/mindmap/
+#docker rm -f mindmap-wisemap
+#docker run -dt --restart unless-stopped --name mindmap-wisemap -p $PORT:8080 -v $BASE_DIR/mindmap:/var/lib/wisemapping/db wisemapping/wisemapping:latest
+
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "#########################################################"
+echo "#########################################################"
+echo " "
+echo " "
+echo "login http://$local_ip:$PORT to access."
+sleep 5
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/mindmap"
---
+++ mycloud-AIO/set_configuration.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+BASE_DIR=/mnt/DriveDATA
+
+#check if docker installed, if not install it
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+
+#Check if nginx is running and download reverse proxy file
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/mycloud-AIO/mycloudaio.conf -o mycloudaio.conf
+mv mycloudaio.conf /etc/nginx/sites-enabled/mycloudaio
+
+## Setting up
+mkdir -p $BASE_DIR/NextcloudAIO/nextcloud_aio_mastercontainer
+mkdir -p $BASE_DIR/NextcloudAIO/ncdata
+docker volume create nextcloud_aio_mastercontainer
+rm -rf /var/lib/docker/volumes/nextcloud_aio_mastercontainer/_data
+ln -s $BASE_DIR/NextcloudAIO/nextcloud_aio_mastercontainer/ /var/lib/docker/volumes/nextcloud_aio_mastercontainer/_data
+
+docker run -dt --sig-proxy=false --name nextcloud-aio-mastercontainer --restart unless-stopped --publish 4000:8080 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config --volume /var/run/docker.sock:/var/run/docker.sock:ro --env SKIP_DOMAIN_VALIDATION=true --env APACHE_PORT=11000 --env APACHE_IP_BINDING=0.0.0.0 --env NEXTCLOUD_DATADIR="$BASE_DIR/NextcloudAIO/ncdata" --env NEXTCLOUD_UPLOAD_LIMIT=1G --env NEXTCLOUD_MAX_TIME=3600 --env NEXTCLOUD_MEMORY_LIMIT=512M --env NEXTCLOUD_STARTUP_APPS="twofactor_totp tasks calendar contacts notes" nextcloud/all-in-one:latest
+
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "************************* For Self hosted please refer***************************************"
+echo "AIO URL, Please access https://$local_ip:4000"
+echo "myCloud URL. Please access https://$local_ip:11000 "
+echo "************************* For Cloud hosted please refer***************************************"
+echo "Please edit /etc/nginx/sites-enabled/mycloudaio file to set server_name in reverse proxy"
+echo "generate SSL by running certbot --nginx"
+echo "If deployed on cloud, Please access URL which is set in reverse proxy for port 4000"
